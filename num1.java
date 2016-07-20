654548d2e0f51c73def97803911e323d365916ce
DharaniArumugam <dharaniarumugamit@gmail.com>
2016-07-20 15:53:32 +0530

    Add files via upload

---
+++ num1.java
@@ -0,0 +1,20 @@
+package githup;
+
+import java.util.Scanner;
+
+public class num1 {
+
+	 public static void main(String args[]){
+		    int a;
+		    Scanner in=new Scanner(System.in);
+		    a=in.nextInt();
+		    if(a%2==0){
+		      System.out.println(a+"is even");
+		    }
+		   
+		    else{
+		      System.out.println(a+"is not evem");
+		    }
+		    
+		  }
+}
