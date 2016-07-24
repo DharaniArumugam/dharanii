cc5e364d3843f049164531c20679e891bb1d7a92
DharaniArumugam <dharaniarumugamit@gmail.com>
2016-07-24 10:15:55 +0530

    Add files via upload

---
+++ str1.java
@@ -0,0 +1,14 @@
+package githup;
+
+public class str1 {
+
+	public static void main(String[] args) {
+
+		String s1 = "w1e2l3c4o5m6e7";
+		String h[]=s1.split("[0-9]");
+		for(String s:h)
+		System.out.println(s);
+		    }
+
+	
+}
